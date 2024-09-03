3a04ac60afa85f606ef910e0d109eda478113043
Tooba Afzali <toobaafzalii78@gmail.com>
2024-09-03 14:45:59 +0330

    ADD onboarding page

---
+++ onBoarding/index.js
@@ -0,0 +1,92 @@
+import "../style.css";
+import { navigateTo } from "../utils/navigation";
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  cssMode: true,
+
+  mousewheel: true,
+});
+console.log(swiper.activeIndex);
+
+const onboardingImg = document.getElementById("onboarding-img");
+const onBoardingText = document.getElementById("onboarding-txt");
+const paginationSection = document.getElementById("onboarding-pagination");
+const onBoardingBtn = document.getElementById("onboarding-btn");
+const swipperWrapper = document.getElementById("swiper-wrapper");
+
+const onBoardingData = [
+  {
+    page: 0,
+    imgSrc: "../public/PNG/onBoarding1.png",
+    text: "We provide high quality products just for you",
+  },
+  {
+    page: 1,
+    imgSrc: "../public/PNG/onBoarding2.png",
+    text: "Your satisfaction is our number one periority",
+  },
+  {
+    page: 2,
+    imgSrc: "../public/PNG/onBoarding3.png",
+    text: "Let’s fulfill your fashion needs with shoearight now!",
+  },
+];
+
+function renderPagination() {
+  paginationSection.innerHTML = "";
+  for (let data of onBoardingData) {
+    let node = `<div id='pagination-${data.page}' class=" w-8 ${
+      data.page !== swiper.activeIndex ? " bg-gray-400" : " bg-App-black"
+    }  h-1"></div>`;
+    paginationSection.innerHTML += node;
+  }
+}
+
+function renderPage() {
+  // const data = onBoardingData[activeIndex];
+  // onboardingImg.src = data.imgSrc;
+  // onBoardingText.textContent = data.text;
+
+  for (let item of onBoardingData) {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const text = document.createElement("div");
+    text.textContent = item.text;
+    text.className =
+      "text-[32px] font-semibold leading-10 flex justify-center items-center text-center mt-8 px-6";
+    img.src = item.imgSrc;
+    img.className = "w-full";
+    div.className = "swiper-slide";
+    div.append(img);
+    div.append(text);
+    swipperWrapper.append(div);
+  }
+}
+addEventListener("DOMContentLoaded", () => {
+  renderPagination();
+  renderPage();
+});
+
+onBoardingBtn.addEventListener("click", () => changeOnboardingData());
+
+function changeOnboardingData() {
+  swiper.slideNext();
+  console.log(swiper.clickedIndex);
+  if (swiper.activeIndex + 1 === onBoardingData.length) {
+    navigateTo("logIn");
+    return;
+  }
+  // renderPage();
+}
+
+swiper.on("slideChange", function (e) {
+  renderPagination();
+  if (swiper.activeIndex + 1 == onBoardingData.length) {
+    onBoardingBtn.textContent = "Get Started";
+  } else {
+    onBoardingBtn.textContent = "Next";
+  }
+});
